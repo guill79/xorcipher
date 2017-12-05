924aa01e402c5b1ce3ca8d111b6695a5b01db036
guill <guillaume.electro@hotmail.fr>
2017-12-05 16:52:29 +0100

    Implémentation des fonctions de utils.h

--- src/utils.c
+++ src/utils.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 #include "../inc/utils.h"
 
+byte **init_2d_array(uint8 length) {
+    byte **array = malloc(length * sizeof(byte *));
+    ALLOC_CHECK(array);
+
+    for (int i = 0; i < length; ++i) {
+        array[i] = malloc(sizeof(byte));
+        ALLOC_CHECK(array[i]);
+    }
+
+    return array;
+}
+
+void expand_array(byte **array, uint32 new_length) {
+    *array = realloc(*array, (new_length + 1) * sizeof(byte));
+    ALLOC_CHECK(*array);
+}
+
+void free_array(byte **array, uint32 length) {
+    free(*array);
+    array = NULL;
+}
+
+void free_2d_array(byte ***array, uint32 length) {
+    for (int i = 0; i < length; ++i) {
+        free((*array)[i]);
+        (*array)[i] = NULL;
+    }
+    free(*array);
+    array = NULL;
+}
+
 uint32 file_to_str(FILE *f, byte *str[]) {
     uint32 f_size = 0;
 
@@ -15,7 +46,7 @@ uint32 file_to_str(FILE *f, byte *str[]) {
     /* Lecture du fichier */
     if (fread(*str, sizeof(byte), f_size, f) != f_size) {
         perror("Erreur lecture fichier");
-        return 0;
+        return -1;
     }
 
     (*str)[f_size] = '\0';
