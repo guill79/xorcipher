5c4cd89553f9472a155c3801850310f8b30c36dd
guill <guillaume.electro@hotmail.fr>
2017-12-14 18:41:47 +0100

    Ajout d'une ligne vide à la fin

--- inc/c1.h
+++ inc/c1.h
@@ -8,9 +8,9 @@ bool char_valid_key(byte c);
 bool test_char_on_str(byte c, uint8 pos, uint8 key_length, byte str[],
                       uint32 str_length);
 void print_chars(byte chars[], uint8 nb_chars);
-void extract_keys_loop(uint8 i, byte **key_chars, byte key_chars_length[],
-                       byte *temp_key, uint8 key_length);
-byte **extract_keys(byte **key_chars, uint8 key_chars_length[],
+void extract_keys_loop(uint8 i, uint16 *i_key, byte **keys, byte **key_chars,
+                       byte key_chars_length[], byte temp_key[], uint8 key_length);
+byte **extract_keys(byte **key_chars, uint16 *nb_keys, uint8 key_chars_length[],
                     uint8 key_length);
 byte **c1(byte str[], uint32 str_length, uint8 key_length,
           uint8 key_chars_length[]);
