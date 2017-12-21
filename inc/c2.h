a060447203f63e6397e22efb3715bc55e89debad
guill <guillaume.electro@hotmail.fr>
2017-12-21 09:34:01 +0100

    Ajout de la fonction char_matches
    Cette fonction prend en paramètre un numéro de lettre entre 0 et 25
    et un caractère. Elle renvoie true si les caractères correspondent
    en prenant en compte les variantes accentuées.

--- inc/c2.h
+++ inc/c2.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+bool char_matches(byte char_num, byte c);
 double frequency_char(byte str[], uint32 str_length, byte c);
 double frequency_analysis(byte str[], uint32 str_length);
 void c2(byte str[], uint32 str_length, byte **keys, uint32 nb_keys);
