b531f853da517f129e416073a99abd4fc0ea49c6
guill <guillaume.electro@hotmail.fr>
2017-12-22 16:05:11 +0100

    Modification des paramètres de la fonction c2
    Elle a besoin de la taille de la clé.

--- inc/c2.h
+++ inc/c2.h
@@ -6,6 +6,6 @@
 bool char_matches(byte char_num, byte c);
 double frequency_char(byte str[], uint32 str_length, byte c);
 double frequency_analysis(byte str[], uint32 str_length);
-void c2(byte str[], uint32 str_length, byte **keys, uint32 nb_keys);
+void c2(byte str_crypted[], uint32 str_length, byte **keys, uint32 nb_keys, uint8 key_length);
 
 #endif // C2_H
