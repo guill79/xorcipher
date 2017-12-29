2e663c2e6928af91d28a4ebd56f4dbbe6abe364d
guill <guillaume.electro@hotmail.fr>
2017-12-29 14:22:37 +0100

    Ajout de constantes

--- inc/c3.h
+++ inc/c3.h
@@ -3,6 +3,10 @@
 
 #include "types.h"
 
+#define NB_WORDS_DICT 336531
+#define MAX_WORD_LENGTH 30
+#define DICT_NAME "fr_dict.txt"
+
 byte **load_dict(char *dict_name, uint16 nb_words, uint8 max_word_length);
 bool is_delimiter(byte c);
 byte **extract_words(byte *str, uint32 str_length, uint32 *nb_words);
