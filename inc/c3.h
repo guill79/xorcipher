64d01f28b85f9d0c352dceabc81929eb2c4e24d8
guill <guillaume.electro@hotmail.fr>
2018-01-02 18:32:53 +0100

    Modification de MAX_WORD_LENGTH

--- inc/c3.h
+++ inc/c3.h
@@ -4,7 +4,7 @@
 #include "types.h"
 
 #define NB_WORDS_DICT 336531
-#define MAX_WORD_LENGTH 30
+#define MAX_WORD_LENGTH 26
 #define DICT_NAME "fr_dict.txt"
 
 byte **load_dict(char *dict_name, uint16 nb_words, uint8 max_word_length, long ****positions);
